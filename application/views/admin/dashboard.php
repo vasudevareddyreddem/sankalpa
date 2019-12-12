aec8e7bed67d63fb8dd54238a4fd514420ebc9ee
bayapucg <bayapu.edu004@gmail.com>
2019-12-12 17:40:19 +0530

    pechat

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,3 +1,42 @@
+<head>
+<script src="https://canvasjs.com/assets/script/jquery.canvasjs.min.js"></script>
+	
+<script>
+window.onload = function () {
+
+var options = {
+	title: {
+		text: "Patient Feeback"
+	},
+	subtitles: [{
+		text: "As of November, 2019"
+	}],
+	animationEnabled: true,
+	data: [{
+		type: "pie",
+		startAngle: 40,
+		toolTipContent: "<b>{label}</b>: {y}%",
+		showInLegend: "true",
+		legendText: "{label}",
+		indexLabelFontSize: 16,
+		indexLabel: "{label} - {y}%",
+		dataPoints: [
+			{ y: 5, label: "Very Poor" },
+			{ y: 10, label: "Poor" },
+			{ y: 35, label: "Average" },
+			{ y: 20, label: "Good" },
+			{ y: 30, label: "Excellent" }
+		]
+	}]
+};
+$("#chartContainer").CanvasJSChart(options);
+$("#chartContainer1").CanvasJSChart(options);
+
+}
+
+</script>
+
+</head>
 <div class="content-wrapper">
 	<!-- Content Header (Page header) -->
 	<section class="content-header">
@@ -35,7 +74,12 @@
 					</div> <a href="<?php echo base_url('employee/presentabsent'); ?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
 				</div>
 			</div>
-			
+			<div class="col-md-6 mt-4">
+				<div id="chartContainer" style="height: 300px; width: 100%;"></div>
+			</div>
+			<div class="col-md-6 mt-4">
+				<div id="chartContainer1" style="height: 300px; width: 100%;"></div>
+			</div>
 			
 			
 			<!-- ./col -->
