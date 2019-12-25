ebd21f22f5f50c060a1847b883ea450c0ccbe6ad
vasudevareddy <vasudevareddy549@gmail.com>
2019-12-25 18:32:13 +0530

    adding

--- application/views/work/list.php
+++ application/views/work/list.php
@@ -2,12 +2,12 @@
 	<!-- Content Header (Page header) -->
 	<section class="content-header">
 		<h1>
-       Assign work List
+       Assign Task List
       </h1>
 		<ol class="breadcrumb">
 			<li><a href="<?php echo base_url('dashboard'); ?>"><i class="fa fa-dashboard"></i> Home</a>
 			</li>
-			<li class="active">Assign work List</li>
+			<li class="active">Assign Task List</li>
 		</ol>
 	</section>
 	<!-- Main content -->
